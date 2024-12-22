3981e277259f9f73d1cacfb11be95fd97f8f0c6b
bodik1987 <bsshul@gmail.com>
2024-12-22 14:56:01 +0100

    Init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ButtonUp from "./components/ui/button-up";
+import ProgressScroll from "./components/ui/progress-scroll";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +29,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
+        <Header />
+        <ButtonUp />
+        <ProgressScroll />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/lib/useTheme.tsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+export default function useTheme() {
+  const prevTheme =
+    typeof window !== "undefined" && localStorage.getItem("theme");
+  const [theme, setTheme] = useState(() => {
+    if (prevTheme) {
+      return prevTheme;
+    }
+    return typeof window !== "undefined" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === "dark") {
+      root.classList.remove("light");
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+      root.classList.add("light");
+    }
+    typeof window !== "undefined" && localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  return { theme, toggleTheme };
+}
