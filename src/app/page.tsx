7b9e1559b88a4a26b11337bc9cd2c62fb7379263
bodik1987 <bsshul@gmail.com>
2024-12-23 17:07:02 +0100

    menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Menu from "./components/ui/menu";
 import ZoomImage from "./components/zoom-image";
 
 export default function Home() {
   return (
     <>
+      <Menu />
       <div className="h-[100vh] p-4">Home</div>
       <ZoomImage />
       <div className="h-[50vh] p-4">Next section</div>
